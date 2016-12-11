1b2ac420ace947086ea6582865cde8313e99b1c3
Amarpal Singh <amarpal@uw.edu>
2016-12-11 01:45:31 -0800

    Updated install-chromiumify.sh script.
    Commented out the installation of Firefox Web Extensions package.

--- install-chromiumify.sh
+++ install-chromiumify.sh
@@ -1,5 +1,5 @@
 sudo apt update && sudo apt -y dist-upgrade
 sudo apt install npm
 sudo apt install nodejs-legacy
-sudo npm install --global web-ext
+# sudo npm install --global web-ext
 sudo npm install chromiumify -g
